197a7f37a314169d91e3d6fbe4bdb7109444f48c
Lenovo pc <Lenovo pc@LAPTOP-0BS7U7FG>
2018-02-18 10:08:35 +0530

    Added default profile

--- osdemo/src/main/java/com/example/zorro/OsController.java
+++ osdemo/src/main/java/com/example/zorro/OsController.java
@@ -1,15 +1,44 @@
 package com.example.zorro;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 
 
 @Controller
 @RequestMapping(path="/osdemo")
 public class OsController {
+	
+	@Autowired
+	OsService carService;
+	
+	
+	@RequestMapping("/insert")
+	public @ResponseBody Iterable<Osdemo> insertCars(@RequestBody Osdemo carsRqst) {		
+		System.out.println("Vehicle #"+carsRqst.getRegNo());
+		return carService.insertCar(carsRqst);
+	}
+		
+	@RequestMapping("/findAll")
+	public @ResponseBody Iterable<Osdemo> findCars() {
+		return carService.findCars();
+	}
+	
+	@RequestMapping("/findOne")
+	public @ResponseBody Osdemo findCar(@RequestParam Integer regno) {
+		return carService.findCar(regno);
+	}
+	
+	@RequestMapping("/remove")
+	public @ResponseBody Osdemo removeCar(@RequestParam Integer regno) {
+		return carService.removeCar(regno);
+	}
 
 	@RequestMapping("/greet")
 	public @ResponseBody String insertCars(@RequestBody String name) {		
